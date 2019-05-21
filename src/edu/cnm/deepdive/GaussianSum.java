06891c26cde1998ab201ec2502f3f6fe1efaff10
Monjoie11 <jeff.franken@gmail.com>
2019-05-21 10:32:43 -0600

    fourth commit using sequencing instead of brute force fixing import of .util with fixing scanner

--- src/edu/cnm/deepdive/GaussianSum.java
+++ src/edu/cnm/deepdive/GaussianSum.java
@@ -6,25 +6,29 @@ import java.util.Scanner;
 public class GaussianSum {
 
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int upperLimit;
-        
-        upperLimit = -1;
-        long sum;
-        
-        while (upperLimit < 1 || upperLimit > 1000000) {
-            upperLimit = input.nextInt();
-            if (upperLimit < 1 || upperLimit > 1000000) {
-                System.out.println("Please input a number greater than 1 or less than 1,000,000");
-            }
-        }
-        sum = 0;
-        int i;
-        
-        for (i = 1; i <= upperLimit; i++) {
-            sum = ((long) upperLimit * (upperLimit + 1)) / 2;
-        }
-        System.out.println("you're totally awesome and the sum is " + sum);
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int upperLimit;
+
+    upperLimit = -1;
+    long sum;
+
+    while (upperLimit < 1 || upperLimit > 1000000) {
+      while (!input.hasNextInt()) {
+        input.next();
+        System.out.println("you no speak good, please enter intergers");
+      }
+      upperLimit = input.nextInt();
+      if (upperLimit < 1 || upperLimit > 1000000) {
+        System.out.println("Please input a number greater than 1 or less than 1,000,000");
+      }
     }
+    sum = 0;
+    int i;
+
+    for (i = 1; i <= upperLimit; i++) {
+      sum = ((long) upperLimit * (upperLimit + 1)) / 2;
+    }
+    System.out.println("you're totally awesome and the sum is " + sum);
+  }
 }
